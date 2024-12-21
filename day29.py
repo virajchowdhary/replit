2f3e6ef551ce3cffc2876aa6f0d471cdebf91394
virajchowdhary <virajvchowdhary@gmail.com>
2024-12-21 13:55:05 -0800

    Stuff

---
+++ day29.py
@@ -0,0 +1,7 @@
+def challenge():
+  print("Super Subroutine\n")
+  print("With my","\033[95m", "new program","\033[0m","I can just")
+  print("call red('and')","\033[31m","and","\033[0m","that word will")
+  print("appear in the color I set it to.\n\n","With no","\033[96m","weird gaps.")
+  print("\033[0m","\n""Epic.")
+challenge()
