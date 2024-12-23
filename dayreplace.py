32a5861f39991f9658bc83874038f07e3fd723fd
virajchowdhary <virajvchowdhary@gmail.com>
2024-12-23 14:46:24 -0800

    One works Day 31

---
+++ dayreplace.py
@@ -0,0 +1,42 @@
+def main():
+  print("                         \033[31m",
+        "=",
+        "\033[37m",
+        "=",
+        "\033[34m",
+        "=",
+        "  Music App  ",
+        "\033[34m",
+        "=",
+        "\033[37m",
+        "=",
+        "\033[31m",
+        "=\n",
+        "\033[0m",
+        sep="")
+  print("🔥▶️  Radio Gaga")
+  print("\033[37m", "Queen\n\n", sep="      ")
+  print("\033[37m", "PREV")
+  print("\033[32m", "NEXT", sep="      ")
+  print("\033[35m", "PAUSE", sep="           ")
+  print("\033[0m")
+  print("\n\n\n")
+
+main()
+def App():
+  W="WELCOME TO"
+  A="\033[34m","--    ARMBOOK    --"
+  L="\033[37m","Username\n","Password"
+  Z="\033[33m","Definitely not a rip off of"
+  X="\033[33m","a cerain other social"
+  C="\033[33m","networking site."
+  H="\033[31m","Honest"
+  Q1=f"{W:>50}"
+  Q2=f"{A:>50}"
+  Q3=f"{Z:>35}"
+  Q4=f"{X:>40}"
+  Q5=f"{C:>60}"
+  Q6=f"{H:>50}"
+  Q7=f"{L:>50}"
+  print(Q1,Q2,"\n",Q3,Q4,Q5,"\n",Q6,"\n",Q7)
+App()
